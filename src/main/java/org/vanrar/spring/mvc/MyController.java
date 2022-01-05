329868020bda3834471674592ef18f060424a473
VanRar <vitalikf7@gmail.com>
2022-01-05 17:16:42 +0300

    add RequestParam in method

--- src/main/java/org/vanrar/spring/mvc/MyController.java
+++ src/main/java/org/vanrar/spring/mvc/MyController.java
@@ -3,6 +3,7 @@ package org.vanrar.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,10 +20,10 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+   //заменяем на  @RequestParam - позволяет связывать поле формы с параметрами метода из контроллера
     @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model) {
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
 
-        String empName = request.getParameter("employeeName");//получаем значения поля employeeName из ask-emp-details-view
         empName = "Mr. " + empName;
         model.addAttribute("nameAttribute", empName);
         return "show-emp-details-view";
